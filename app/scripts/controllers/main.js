ac339bd5ea10fd45abfe2e9185f948f9d6220818
Ishan <contact@ishansharma.com>
2017-09-16 11:11:15 -0500

    Formatting JS

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,10 @@
  * Controller of the appBApp
  */
 angular.module('appBApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
