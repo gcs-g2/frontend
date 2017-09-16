958295189ede7eaff04c3171bcb3ecd731a5aaf6
Ishan <contact@ishansharma.com>
2017-09-16 11:07:42 -0500

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appBApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appBApp
+ */
+angular.module('appBApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appBApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appBApp
+ */
+angular.module('appBApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
